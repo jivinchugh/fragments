b35451e5fcbdd5a23fc254f904f859149635652f
Jivin Chugh <jivinchugh18@gmail.com>
2024-09-18 10:25:40 -0400

    health check route updated to use createSuccessResponse() from src/response.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ const express = require('express');
 // version and author from package.json
 const { version, author } = require('../../package.json');
 
+//import createSuccessResponse from package.json
+const { createSuccessResponse } = require('../response');
+
 // Create a router that we can use to mount our API
 const router = express.Router();
 // modifications to src/routes/index.js
@@ -28,13 +31,15 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/jivinchugh/fragments.git',
-    version,
-  });
+  res.status(200).json(
+    createSuccessResponse({
+      //status: 'ok',
+      author,
+      // Use your own GitHub URL for this!
+      githubUrl: 'https://github.com/jivinchugh/fragments.git',
+      version,
+    })
+  );
 });
 
 module.exports = router;
