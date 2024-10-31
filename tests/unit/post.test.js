da68c37ecd852a922612dc8aa884bfea0a30dec8
Jivin Chugh <jivinchugh18@gmail.com>
2024-10-30 22:02:43 -0400

    test-cases added for post and /:id/info

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -48,7 +48,7 @@ describe('POST /v1/fragments', () => {
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
-      .set('Content-Type', 'application/json') // Change this to any unsupported type
+      .set('Content-Type', 'image/png') // Change this to any unsupported type
       .send('{}'); // Send an empty JSON object or a string
 
     const body = JSON.parse(res.text);
