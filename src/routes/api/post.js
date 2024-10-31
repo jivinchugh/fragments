78898d8d15e2f9f41686a920465560d27b58d026
Jivin Chugh <jivinchugh18@gmail.com>
2024-10-30 22:01:54 -0400

    create text/* or application/json fragment implementation

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,40 +1,35 @@
+// src/routes/api/post.js
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 
 module.exports = async (req, res) => {
   logger.info(`Received POST request from user ${req.user}`);
-
-  // Ensure req.body is a Buffer
-  const fragData = req.body;
-  if (!Buffer.isBuffer(fragData)) {
-    logger.warn('Unsupported content format. Expected Buffer.');
-    return res.status(415).json(createErrorResponse(415, 'The content format for fragment (supplied by client) is not supported!!'));
-  }
-
   const { user: ownerId } = req;
   const contentType = req.get('Content-Type');
+  const fragData = contentType === 'text/plain' || contentType === 'text/markdown' || contentType === 'text/html' || contentType === 'text/csv' ? req.body : JSON.stringify(req.body);
 
-  // Validate the Content-Type for text/* or application/json
-  if (!contentType.startsWith('text/') && contentType !== 'application/json') {
-    logger.warn(`Unsupported content-type: ${contentType}`);
-    return res.status(415).json(createErrorResponse(415, 'Unsupported content-type. Allowed: text/*, application/json'));
+  /*if (!Buffer.isBuffer(fragData)) {
+    logger.warn('Unsupported content format. Expected Buffer.');
+    return res.status(415).json(createErrorResponse(415, 'The content format for fragment (supplied by client) is not supported!!'));
+  }*/
+
+  // Check if the Content-Type is supported
+  if (!Fragment.isSupportedType(contentType)) {
+    logger.warn(`Unsupported media type: ${contentType}`);
+    return res.status(415).json(createErrorResponse(415, 'The content format for fragment (supplied by client) is not supported!!'));
   }
 
   try {
     logger.debug('Attempting to create a new fragment');
     const fragment = new Fragment({ ownerId, type: contentType });
     await fragment.save();
-    await fragment.setData(fragData);
+    await fragment.setData(Buffer.from(fragData)); // Ensure data is a Buffer
     logger.info(`Fragment metadata and data saved. Owner: ${ownerId}, ID: ${fragment.id}, Size: ${fragment.size} bytes`);
-    res.set('Content-Type', fragment.type);
-    // Construct the Location URL using req.protocol and req.headers.host
-    //const APIURL = process.env.API_URL;
-    //const locationURL = `${APIURL}/v1/fragments/${fragment.id}`;
-    // here the logic ${APIURL} is same as ${req.protocol}://${req.headers.host}
+
     const locationURL = `${req.protocol}://${req.headers.host}/v1/fragments/${fragment.id}`;
     res.set('Location', locationURL);
-    logger.debug(`Location header set: ${locationURL}`);
+
     res.status(201).location(locationURL).json(
       createSuccessResponse({
         fragment: {
@@ -44,7 +39,7 @@ module.exports = async (req, res) => {
           updated: fragment.updated,
           type: fragment.type,
           size: fragment.size,
-        }
+        },
       })
     );
     logger.info(`Fragment created successfully for user ${ownerId}, ID: ${fragment.id}`);
