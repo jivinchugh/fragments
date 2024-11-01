723912649866739f4f2d2959dd9e91a4744eee47
Jivin Chugh <jivinchugh18@gmail.com>
2024-11-01 16:34:41 -0400

    removed extension params

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -6,7 +6,7 @@ const logger = require('../../logger');
 
 module.exports = async (req, res) => {
   const { user: ownerId } = req;
-  const { id, ext } = req.params;
+  const { id } = req.params;
   try {
     logger.info(`Fetching fragment by ID: ${id}`);
 
@@ -16,17 +16,6 @@ module.exports = async (req, res) => {
       logger.warn(`Fragment not found for ID: ${id}`);
       return res.status(404).json(createErrorResponse(404, 'Fragment not found'));
     }
-    if (ext) {
-      const { convertedData, convertedType } = await fragment.convertType(await fragment.getData(), ext);
-      if (!convertedData) {
-        return res.status(415).json(createErrorResponse(415, 'Fragment can\'t be converted to this type or extension is invalid'));
-      }
-      // Return the converted fragment data
-      logger.info(`Successfully converted and fetched fragment: ${id}`);
-      res.set('Content-Type', convertedType);
-      return res.status(200).json(createSuccessResponse({ content: convertedData }));
-    }
-
     logger.info(`Successfully fetched fragment: ${JSON.stringify(fragment)}`);
     res.status(200).json(createSuccessResponse({ fragment }));
   } catch (error) {
